d34959bdf46d0cfcd551674ddc78897d9b23c774
anystedt1 <andreanystedt@gmail.com>
2026-05-04 15:16:47 -0400

    Fix boundary checks and improve mine placement logic

--- Minesweeper.py
+++ Minesweeper.py
@@ -11,7 +11,7 @@ def draw_square(x, y, number): # make a bunch of squares
   
     if number == -1: # creating labels (remember -1 or mine)
        num_or_mine = Label(number,square.centerX, square.centerY, fill="white", bold=True) # make text white for number away essentially
-    
+
     else:
         num_or_mine = Circle(square.centerX,square.centerY,5, fill='red')
     flag = Polygon(square.centerX-5,square.centerY-5,square.centerX+7,square.centerY-2, square.centerX-5,square.centerY+5, fill='yellow') # if there is no number then its either a mine or you can double click for a flag
@@ -22,7 +22,7 @@ def draw_square(x, y, number): # make a bunch of squares
             flag
     )   
     square_with_mine_or_num.content = num_or_mine  # custom attribute content
-    square_with_mine_or_num.content.visible = False
+    square_with_mine_or_num.content.visible = True #invisible trait
     square_with_mine_or_num.flag = flag  # custom attribute flag which can be added on
     square_with_mine_or_num.flag.visible = False # invisible flag
     return square_with_mine_or_num # returns the value of what the squares "secret is"
@@ -33,7 +33,7 @@ def initialize_numbers(): # changes -1 with the amount of mines around the given
     for row in range(rows): # kind of like the images that we did we need to go through the grid like this to find the exact distance between the mine and the spot chosen
         for col in range(cols):
             #inner rows and columns
-            if row !=0 and row !=8 and col !=0 and col !=8:  
+            if row !=0 and row != rows -1 and col !=0 and col != cols -1:  
                 if isinstance(board[row-1][col-1].content, Circle):
                    neighbor_mines += 1 
                 if isinstance(board[row-1][col].content, Circle):
@@ -60,7 +60,7 @@ def initialize_numbers(): # changes -1 with the amount of mines around the given
                 if isinstance(board[row+1][col].content, Circle):
                    neighbor_mines += 1 
             #firts row without the corners
-            elif row == 0 and col!=0 and col <8: 
+            elif row == 0 and col!=0 and col != cols-1: 
                 if isinstance(board[row][col+1].content, Circle):
                    neighbor_mines += 1 
                 if isinstance(board[row+1][col+1].content, Circle):
@@ -80,7 +80,7 @@ def initialize_numbers(): # changes -1 with the amount of mines around the given
                 if isinstance(board[row][col+1].content, Circle):
                    neighbor_mines += 1         
             #last row without the corners
-            elif row == 8 and col!=0 and col <8: 
+            elif row == 8 and col!=0 and col != cols-1: 
                 if isinstance(board[row][col-1].content, Circle):
                    neighbor_mines += 1 
                 if isinstance(board[row-1][col-1].content, Circle):
@@ -92,7 +92,7 @@ def initialize_numbers(): # changes -1 with the amount of mines around the given
                 if isinstance(board[row][col+1].content, Circle):
                    neighbor_mines += 1 
             #bottom-right corner   
-            elif row == 8 and col == 8:    
+            elif row == rows-1 and col == cols-1:    
                 if isinstance(board[row-1][col].content, Circle):
                    neighbor_mines += 1 
                 if isinstance(board[row-1][col-1].content, Circle):
@@ -100,43 +100,46 @@ def initialize_numbers(): # changes -1 with the amount of mines around the given
                 if isinstance(board[row][col-1].content, Circle):
                    neighbor_mines += 1   
             #finish top-right corner                #COME BACK AND FINISH OFF!!!
+            elif row ==0 and col == cols -1:
+                if isinstance(board[row][col-1].content, Circle):
+                    neighbor_mines += 1
+                if isinstance(board[row+1][col-1].content, Circle):
+                    neighbor_mines += 1
+                if isinstance(board[row+1][col].content, Circle): 
+                    neighbor_mines += 1
             # finish first column without corners 
+            elif col == 0 and row !=0 and row != rows-1:
+                if isinstance(board[row-1][col].content, Circle):
+                    neighbor_mines += 1 
+                if isinstance(board[row-1][col+1].content, Circle):
+                    neighbor_mines += 1     
+                if isinstance(board[row][col+1].content, Circle):
+                    neighbor_mines += 1     
+                if isinstance(board[row+1][col+1].content, Circle):
+                    neighbor_mines += 1   
+                if isinstance(board[row+1][col].content, Circle):
+                    neighbor_mines += 1
+            
             # finish last column withou corners    
+            elif col == cols-1 and row!=0 and row!= rows-1:
+                if isinstance(board[row-1][col-1].content, Circle):
+                    neighbor_mines += 1     
+                if isinstance(board[row-1][col].content, Circle):
+                    neighbor_mines += 1
+                if isinstance(board[row][col-1].content, Circle):
+                    neighbor_mines += 1
+                if isinstance(board[row+1][col-1].content, Circle):
+                    neighbor_mines += 1
+                if isinstance(board[row+1][col].content, Circle):
+                    neighbor_mines += 1
 
             board[row][col].content.value = neighbor_mines
-            print(neighbor_mines) 
+            #print(neighbor_mines) 
             neighbor_mines = 0
             
       
-
-rows = 9
-cols = 9
-#  Making the board - LIST OF LISTS!!! 
-# for each row there will be a same number of columnts
-board = [ ([0] * cols) for row in range(rows) ] 
-
-mine_count = 0 # starts at 0 cause need to randomly pla
-for i in range(rows):
-    for j in range(cols):
-        random_mine = random.randint(0,4) # placing the mines when randomly generates a 0 it will put a mine on the map
-        if random_mine == 0 and mine_count<10: # 10 mines
-            mine_count +=1 # adds to the mine count
-            board[i][j] = draw_square(j* 30+ 70, i*30 + 70, 0) # draw square is the function I defined in the beginning - draws it @ specific coordinate and gives it a # square will have a 0 cause its a mine
-        #when no mine
-        else:
-            board[i][j] = draw_square(j* 30+ 70, i*30 + 70, -1)    # -1 means number so no mine at the spot
-Label("Minecount: ",40,30)
-mine_count_label = Label(mine_count, 80,30)
-initialize_numbers() # how you find how many mines are around it and put in the label that was created in 
-
-     
-eventLabel = Label('', 200, 150, size=40)
-buttonsLabel = Label('', 200, 250, size=40)
-
-
-
     
-# PLAYING Mouse clickes
+
 def onMousePress(mouseX, mouseY, button):
     for row in range(rows): 
         for col in range(cols): 
@@ -144,9 +147,13 @@ def onMousePress(mouseX, mouseY, button):
                 if (button == 0): # button is 0 means left click 
                     board[row][col].flag.visible = False # each square has an invisible flag so when clicked if double it will reveal
                     if isinstance(board[row][col].content,Circle) == True:
-                        
-                        board[row][col].content.fill= 'red'  #end the game?
-                        board[row][col].content.visible = True
+                        if click_counter == 0:   #click_counter
+                            place_mine()
+                            initialize_numbers()
+                        else:
+                            click_counter +=1
+                            board[row][col].content.fill= 'red'  #end the game?
+                            board[row][col].content.visible = True
                     else: # button is 1 will be right click
                         board[row][col].content.visible = True
 
@@ -161,7 +168,35 @@ def onMousePress(mouseX, mouseY, button):
                         board[row][col].content.visible = True
                         
                        
-                     
+def place_mine():
+    # USE fisher-yates shuffle (shuffle index len rowxcol and pick first 10 to be mines)
+    mine_count = 0 # starts at 0 cause need to randomly pla
+    for i in range(rows):
+        for j in range(cols):
+            random_mine = random.randint(0,4) # placing the mines when randomly generates a 0 it will put a mine on the map
+            if random_mine == 0 and mine_count<10: # 10 mines
+                mine_count +=1 # adds to the mine count
+                board[i][j] = draw_square(j* 30+ 70, i*30 + 70, 0) # draw square is the function I defined in the beginning - draws it @ specific coordinate and gives it a # square will have a 0 cause its a mine
+            #when no mine
+            else:
+                board[i][j] = draw_square(j* 30+ 70, i*30 + 70, -1)    # -1 means number so no mine at the spot
+    Label("Minecount: ",40,30)
+    mine_count_label = Label(mine_count, 80,30)
+    initialize_numbers() # how you find how many mines are around it and put in the label that was created in 
+
+        
+    eventLabel = Label('', 200, 150, size=40)
+    buttonsLabel = Label('', 200, 250, size=40)
+
+
+rows = 9
+cols = 9
+click_counter = 0 
+#  Making the board - LIST OF LISTS!!! 
+# for each row there will be a same number of columnts
+board = [ ([0] * cols) for row in range(rows) ] 
+
+place_mine()        
                    
-cmu_graphics.run() # make it good
+cmu_graphics.run()
 
